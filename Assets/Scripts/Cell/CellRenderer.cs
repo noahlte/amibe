0bf2868fba95926fc0232a78f5787473f4ed1b26
noahlte <noahloute1995@gmail.com>
2026-04-19 23:47:34 +0200

    feat: create the cell render with perlin noise and movement

---
+++ Assets/Scripts/Cell/CellRenderer.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+
+public class CellRenderer : MonoBehaviour
+{
+    // Circle
+
+    [Header("Circle Size")]
+    [SerializeField] private int numberOfPoints = 15;
+    [SerializeField] private float radius = 5f;
+    [SerializeField] private float minRadius = 2f;
+
+    // Perlin noise variable
+
+    [Header("Perlin Noise")]
+    [SerializeField] private float xIncrement = 0.2f;
+    [SerializeField] private float yIncrement = 0.15f;
+    [SerializeField] private float timeIncrement = 0.1f;
+
+    private float xoff;
+    private float yoff;
+
+    // Points
+    private Vector2[] points;
+
+    private LineRenderer lr;
+
+    void Start()
+    {
+        lr = gameObject.GetComponent<LineRenderer>();
+        lr.positionCount = numberOfPoints;
+
+        points = new Vector2[numberOfPoints];
+        xoff = Random.Range(0, 10000);
+        yoff = Random.Range(0, 10000);
+
+        points = CalculateCoordonate();
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < numberOfPoints; i++)
+        {
+            lr.SetPosition(i, points[i]);
+        }
+
+        xoff += timeIncrement * Time.deltaTime;
+        yoff += timeIncrement * Time.deltaTime;
+
+        points = CalculateCoordonate();
+    }
+
+    private Vector2[] CalculateCoordonate()
+    {
+        Vector2[] tempPoints = new Vector2[numberOfPoints];
+
+        float startingXoff = xoff;
+        float startingYoff = yoff;
+
+        for (int i = 0; i < numberOfPoints; i++)
+        {
+            // Calcul des coordonnées polaires
+            float pointAngle = ((float)i / (float)numberOfPoints) * (2 * Mathf.PI);
+            float distance = Mathf.PerlinNoise(xoff, yoff);
+            distance = Utils.Map(distance, 0f, 1f, minRadius, radius);
+
+            // Conversion en X et Y
+            float x = transform.position.x + (distance * Mathf.Cos(pointAngle));
+            float y = transform.position.y + (distance * Mathf.Sin(pointAngle));
+
+            tempPoints[i] = new Vector2(x, y);
+
+            xoff += xIncrement;
+            yoff += yIncrement;
+        }
+
+        xoff = startingXoff;
+        yoff = startingYoff;
+
+        return tempPoints;
+    }
+}
