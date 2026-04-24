ce3ac7e3b63061f5f6e2acdb3445689fd9a291fc
noahlte <noahloute1995@gmail.com>
2026-04-24 03:24:51 +0200

    feat: add BaseState class to avoid code duplication

---
+++ Assets/Scripts/State/BaseState.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public abstract class BaseState : MonoBehaviour
+{
+    protected FoodManager fm;
+    private CellSteering cs;
+    protected CellCore cc;
+
+    private bool hasTarget;
+    private Vector3 targetPosition;
+
+    protected abstract void Eat(GameObject target);
+    protected abstract bool CanTarget(GameObject target);
+
+    private void Awake()
+    {
+        cs = gameObject.GetComponent<CellSteering>();
+        cc = gameObject.GetComponent<CellCore>();
+    }
+
+    private void Start()
+    {
+        fm = FindAnyObjectByType<FoodManager>();
+    }
+
+    private void Update()
+    {
+        if (hasTarget)
+        {
+            cs.SteerTo(targetPosition);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (CanTarget(collision.gameObject) && !hasTarget)
+        {
+            cs.StopSeeking();
+            hasTarget = true;
+            targetPosition = collision.transform.position;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        if (CanTarget(other.gameObject)  && hasTarget)
+        {
+            hasTarget = false;
+            cs.StartSeeking();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (CanTarget(collision.gameObject))
+        {
+            Eat(collision.gameObject);
+        }
+    }
+}
--- Assets/Scripts/State/HerbivorState.cs
+++ Assets/Scripts/State/HerbivorState.cs
@@ -1,60 +1,16 @@
 using UnityEngine;
 
-public class HerbivorState : MonoBehaviour
+public class HerbivorState : BaseState
 {
-    private FoodManager fm;
-    private CellSteering cs;
-    private CellCore cc;
-
-    private bool hasTarget;
-    private Vector3 targetPosition;
-
-    private void Awake()
+    protected override bool CanTarget(GameObject target)
     {
-        cs = gameObject.GetComponent<CellSteering>();
-        cc = gameObject.GetComponent<CellCore>();
+        return target.TryGetComponent(out FoodCore fc);
     }
 
-    private void Start()
+    protected override void Eat(GameObject target)
     {
-        fm = FindAnyObjectByType<FoodManager>();
-    }
-
-    private void Update()
-    {
-        if (hasTarget)
-        {
-            cs.SteerTo(targetPosition);
-        }
-    }
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.TryGetComponent(out FoodCore fc) && !hasTarget)
-        {
-            cs.StopSeeking();
-            hasTarget = true;
-            targetPosition = collision.transform.position;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other) 
-    {
-        if (other.TryGetComponent(out FoodCore fc) && hasTarget)
-        {
-            hasTarget = false;
-            cs.StartSeeking();
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.TryGetComponent(out FoodCore fc))
-        {
-            cc.Eat(fc.GetFoodToAdd());
-            Destroy(collision.gameObject);
-            fm.ChangeCurrentFood(-1);
-            //hasTarget = false;
-        }
+        cc.Eat(target.GetComponent<FoodCore>().GetFoodToAdd());
+        Destroy(target);
+        fm.ChangeCurrentFood(-1);
     }
 }
